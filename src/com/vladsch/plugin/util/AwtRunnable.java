8ea1d4c45bf494c1e84f5e0f719212c2b46e2285
Vladimir Schneider <vladimir@vladsch.com>
2019-01-17 18:18:50 -0500

    add optional ModalityState to AwtRunnable

--- src/com/vladsch/plugin/util/AwtRunnable.java
+++ src/com/vladsch/plugin/util/AwtRunnable.java
@@ -23,7 +23,9 @@
 package com.vladsch.plugin.util;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static javax.swing.SwingUtilities.isEventDispatchThread;
 
@@ -33,6 +35,7 @@ import static javax.swing.SwingUtilities.isEventDispatchThread;
 public class AwtRunnable implements Runnable {
     final private Runnable myCommand;
     final private boolean myAwtThread;
+    final private ModalityState myModalityState;
 
     public Runnable getCommand() {
         return myCommand;
@@ -43,40 +46,52 @@ public class AwtRunnable implements Runnable {
     }
 
     public AwtRunnable(Runnable command) {
-        this(false, command);
+        this(false, command, null);
+    }
+
+    public AwtRunnable(Runnable command, ModalityState modalityState) {
+        this(false, command, modalityState);
     }
 
     public AwtRunnable(boolean awtThread, Runnable command) {
+        this(awtThread, command, null);
+    }
+
+    public AwtRunnable(boolean awtThread, Runnable command, ModalityState modalityState) {
         myCommand = command;
         myAwtThread = awtThread;
+        myModalityState = modalityState;
     }
 
     @Override
     public void run() {
         if (myAwtThread && !isEventDispatchThread()) {
-            //ApplicationManager.getApplication().invokeLater(this, ModalityState.any());
-            ApplicationManager.getApplication().invokeLater(this);
+            if (myModalityState != null) ApplicationManager.getApplication().invokeLater(this, myModalityState);
+            else ApplicationManager.getApplication().invokeLater(this);
         } else {
             myCommand.run();
         }
     }
 
+    public static CancellableRunnable schedule(final CancelableJobScheduler scheduler, @NotNull String id, int delay, @NotNull Runnable command) {
+        return schedule(scheduler, id, delay, null, command);
+    }
+
     /**
      * Creates a one-shot runnable that will run after a delay, can be run early, or cancelled
      * <p>
-     * the given command will only be executed once, either by the delayed trigger or by the run method.
-     * if you want to execute the task early just invoke #run, it will do nothing if the task has already run.
-     *
+     * the given command will only be executed once, either by the delayed trigger or by the run method. if you want to execute the task early just invoke #run, it will do nothing if the task has already run.
      *
      * @param scheduler
-     * @param delay   the time from now to delay execution
-     * @param command the task to execute
-     * @return a {@link CancellableRunnable} which will run after the given
-     * delay on the AwtThread if {@link #run()} is invoked before {@link CancellableRunnable#cancel()}
+     * @param delay     the time from now to delay execution
+     * @param command   the task to execute
+     *
+     * @return a {@link CancellableRunnable} which will run after the given delay on the AwtThread if {@link #run()} is invoked before {@link CancellableRunnable#cancel()}
+     *
      * @throws NullPointerException if command is null
      */
-    public static CancellableRunnable schedule(final CancelableJobScheduler scheduler, @NotNull String id, int delay, @NotNull Runnable command) {
-        AwtRunnable runnable = new AwtRunnable(true, command);
+    public static CancellableRunnable schedule(final CancelableJobScheduler scheduler, @NotNull String id, int delay, @Nullable ModalityState modalityState, @NotNull Runnable command) {
+        AwtRunnable runnable = new AwtRunnable(true, command, modalityState);
         return scheduler.schedule(id, delay, runnable);
     }
 }
