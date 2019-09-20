e060589284b6f22639c3d223a782eaedb717b369
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-09-20 00:52:16 -0400

    update for new settings

--- update-plugin.sh
+++ update-plugin.sh
@@ -19,8 +19,8 @@ cp plugin-util.jar ../CLionArduinoPlugin/lib
 cp lib/flexmark-util.jar ../CLionArduinoPlugin/lib
 echo updated plugin-util.jar in ../CLionArduinoPlugin/lib
 
-cp plugin-util.jar ../idea-multimarkdown3/lib
-echo updated plugin-util.jar in ../idea-multimarkdown3/lib
+#cp plugin-util.jar ../idea-multimarkdown3/lib
+#echo updated plugin-util.jar in ../idea-multimarkdown3/lib
 
 cp plugin-util.jar ../idea-multimarkdown2/lib
 echo updated plugin-util.jar in ../idea-multimarkdown2/lib
