b6f137380b9a8c5ae89f7591b7574e0660334a0f
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-09-21 19:22:06 -0400

    add lazy computable/function/runnable kotlin classes for use in java

---
+++ src/com/vladsch/plugin/util/LazyFunction.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015-2019 Vladimir Schneider <vladimir.schneider@gmail.com>, all rights reserved.
+ *
+ * This code is private property of the copyright holder and cannot be used without
+ * having obtained a license or prior written permission of the of the copyright holder.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package com.vladsch.plugin.util
+
+import java.util.function.Function
+
+class LazyFunction<T : Any, R : Any>(val computable: (T) -> R) {
+    constructor(computable: Function<T, R>) : this({ computable.apply(it) })
+
+    // getting this value will do the computation on first request
+    private var argument: T? = null
+
+    private val _value: R by lazy {
+        computable(argument!!)
+    }
+
+    /**
+     * Always returns result from first computation
+     */
+    fun getValue(argument:T):R  {
+        this.argument = argument
+        return _value;
+    }
+}
