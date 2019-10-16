0412643aec8a09cf4dd545dc584c0542b2974724
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-10-15 22:00:15 -0400

    update flexmark-java

--- update-plugin.sh
+++ update-plugin.sh
@@ -8,9 +8,10 @@ cp lib/flexmark-util.jar "/Volumes/Pegasus/Data"
 cp lib/flexmark-tree-iteration.jar "/Volumes/Pegasus/Data"
 echo updated plugin-util.jar in "/Volumes/Pegasus/Data"
 
-#cp plugin-util.jar ../MissingInActions/lib
-#cp lib/flexmark-util.jar ../MissingInActions/lib
-#echo updated plugin-util.jar in ../MissingInActions/lib
+cp plugin-util.jar ../MissingInActions/lib
+cp lib/flexmark-util.jar ../MissingInActions/lib
+cp lib/flexmark-tree-iteration.jar ../MissingInActions/lib
+echo updated plugin-util.jar in ../MissingInActions/lib
 
 cp plugin-util.jar ../touch-typists-completion-caddy/lib
 cp lib/flexmark-util.jar ../touch-typists-completion-caddy/lib
@@ -22,8 +23,10 @@ cp lib/flexmark-util.jar ../CLionArduinoPlugin/lib
 cp lib/flexmark-tree-iteration.jar ../CLionArduinoPlugin/lib
 echo updated plugin-util.jar in ../CLionArduinoPlugin/lib
 
-#cp plugin-util.jar ../idea-multimarkdown3/lib
-#echo updated plugin-util.jar in ../idea-multimarkdown3/lib
+cp plugin-util.jar ../idea-multimarkdown3/lib
+cp lib/flexmark-util.jar ../idea-multimarkdown3/lib
+cp lib/flexmark-tree-iteration.jar ../idea-multimarkdown3/lib
+echo updated plugin-util.jar in ../idea-multimarkdown3/lib
 
 #cp plugin-util.jar ../idea-multimarkdown2/lib
 #cp lib/flexmark-util.jar ../idea-multimarkdown2/lib
