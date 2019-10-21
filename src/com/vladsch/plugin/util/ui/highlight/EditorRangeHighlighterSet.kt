3c3f7aabbd42bfce4b2c16d75e5f9ed59a4c7fa1
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-10-21 11:43:23 -0400

    change highlighter type to RangeHighlighterEx

--- src/com/vladsch/plugin/util/ui/highlight/EditorRangeHighlighterSet.kt
+++ src/com/vladsch/plugin/util/ui/highlight/EditorRangeHighlighterSet.kt
@@ -17,14 +17,15 @@ package com.vladsch.plugin.util.ui.highlight
 
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.ex.RangeHighlighterEx
 import com.intellij.openapi.editor.markup.HighlighterLayer
 import com.intellij.openapi.editor.markup.HighlighterTargetArea
 import com.intellij.openapi.editor.markup.RangeHighlighter
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.vladsch.plugin.util.rangeLimit
 
-class EditorRangeHighlighterSet(val editor: Editor) : MutableSet<RangeHighlighter> {
-    private val rangeHighlighters = LinkedHashSet<RangeHighlighter>()
+class EditorRangeHighlighterSet(val editor: Editor) : MutableSet<RangeHighlighterEx> {
+    private val rangeHighlighters = LinkedHashSet<RangeHighlighterEx>()
 
     private fun removeHighlighter(element: RangeHighlighter): Boolean {
         if (rangeHighlighters.contains(element)) {
@@ -40,7 +41,7 @@ class EditorRangeHighlighterSet(val editor: Editor) : MutableSet<RangeHighlighte
         attributesKey: TextAttributesKey,
         highlighterLayer: Int = HighlighterLayer.ELEMENT_UNDER_CARET,
         targetArea: HighlighterTargetArea = HighlighterTargetArea.EXACT_RANGE
-    ): RangeHighlighter {
+    ): RangeHighlighterEx {
         val attributes = editor.colorsScheme.getAttributes(attributesKey)
         return addRangeHighlighter(startOffset, endOffset, attributes, highlighterLayer, targetArea)
     }
@@ -51,17 +52,17 @@ class EditorRangeHighlighterSet(val editor: Editor) : MutableSet<RangeHighlighte
         attributes: TextAttributes,
         highlighterLayer: Int = HighlighterLayer.ELEMENT_UNDER_CARET,
         targetArea: HighlighterTargetArea = HighlighterTargetArea.EXACT_RANGE
-    ): RangeHighlighter {
+    ): RangeHighlighterEx {
         val rangeHighlighter = editor.markupModel.addRangeHighlighter(startOffset, endOffset.rangeLimit(0, editor.document.textLength), highlighterLayer, attributes, targetArea)
-        rangeHighlighters.add(rangeHighlighter)
-        return rangeHighlighter
+        rangeHighlighters.add(rangeHighlighter as RangeHighlighterEx)
+        return rangeHighlighter as RangeHighlighterEx
     }
 
-    override fun add(element: RangeHighlighter): Boolean {
+    override fun add(element: RangeHighlighterEx): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun addAll(elements: Collection<RangeHighlighter>): Boolean {
+    override fun addAll(elements: Collection<RangeHighlighterEx>): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -70,21 +71,21 @@ class EditorRangeHighlighterSet(val editor: Editor) : MutableSet<RangeHighlighte
         rangeHighlighters.clear()
     }
 
-    override fun iterator(): MutableIterator<RangeHighlighter> {
+    override fun iterator(): MutableIterator<RangeHighlighterEx> {
         return rangeHighlighters.iterator()
     }
 
-    override fun remove(element: RangeHighlighter): Boolean {
+    override fun remove(element: RangeHighlighterEx): Boolean {
         removeHighlighter(element)
         return rangeHighlighters.remove(element)
     }
 
-    override fun removeAll(elements: Collection<RangeHighlighter>): Boolean {
+    override fun removeAll(elements: Collection<RangeHighlighterEx>): Boolean {
         elements.forEach { removeHighlighter(it) }
         return rangeHighlighters.removeAll(elements)
     }
 
-    override fun retainAll(elements: Collection<RangeHighlighter>): Boolean {
+    override fun retainAll(elements: Collection<RangeHighlighterEx>): Boolean {
         val toRemove = rangeHighlighters.filter { elements.contains(it) }
         return removeAll(toRemove)
     }
@@ -92,11 +93,11 @@ class EditorRangeHighlighterSet(val editor: Editor) : MutableSet<RangeHighlighte
     override val size: Int
         get() = rangeHighlighters.size
 
-    override fun contains(element: RangeHighlighter): Boolean {
+    override fun contains(element: RangeHighlighterEx): Boolean {
         return rangeHighlighters.contains(element)
     }
 
-    override fun containsAll(elements: Collection<RangeHighlighter>): Boolean {
+    override fun containsAll(elements: Collection<RangeHighlighterEx>): Boolean {
         return rangeHighlighters.containsAll(elements)
     }
 
