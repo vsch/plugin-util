b777d8abf88847f56833fd8fa502b1d52ee59e37
Vladimir Schneider <vladimir.schneider@gmail.com>
2019-12-18 14:00:13 -0500

    add element type test for elements in table

--- src/com/vladsch/plugin/util/psi/PsiUtils.java
+++ src/com/vladsch/plugin/util/psi/PsiUtils.java
@@ -7,13 +7,38 @@ import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class PsiUtils {
+    static class Holder {
+        final static HashSet<String> TYPE_PREFIXES = new HashSet<>(Arrays.asList(
+                "TABLE_CELL_",
+                "TABLE_HDR_"
+        ));
+    }
+
+    public static boolean isTypeOfSuffix(@Nullable IElementType elementType, @NotNull TokenSet tokenSet) {
+        if (elementType != null) {
+            String typeName = elementType.toString();
+
+            if (typeName.startsWith("TABLE_CELL_") || typeName.startsWith("TABLE_HDR_")) {
+                for (IElementType types : tokenSet.getTypes()) {
+                    if (typeName.endsWith("_" + types.toString())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     public static boolean isTypeOf(@Nullable IElementType elementType, @NotNull TokenSet tokenSet) {
-        return tokenSet.contains(elementType);
+        return tokenSet.contains(elementType) || isTypeOfSuffix(elementType, tokenSet);
     }
 
     public static boolean isNullOrTypeOf(@Nullable IElementType elementType, @NotNull TokenSet tokenSet) {
-        return elementType == null || tokenSet.contains(elementType);
+        return elementType == null || isTypeOf(elementType, tokenSet);
     }
 
     public static boolean isTypeOf(@Nullable ASTNode node, @NotNull TokenSet tokenSet) {
