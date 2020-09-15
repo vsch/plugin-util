0b0c551eaad569e12550271902a2c56260eb2b93
Vladimir Schneider <vladimir.schneider@gmail.com>
2020-09-14 20:03:16 -0400

    update to 3.0.192.110

--- src/main/java/com/vladsch/plugin/util/UtilBundle.java
+++ src/main/java/com/vladsch/plugin/util/UtilBundle.java
@@ -4,13 +4,14 @@ import com.intellij.BundleBase;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.PropertyKey;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class UtilBundle {
     @NonNls
     protected static final String BUNDLE_NAME = "com.vladsch.plugin.util.localization.strings";
 
-    protected static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+    protected static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault(), UtilBundle.class.getClassLoader());
 
     private UtilBundle() {
     }
